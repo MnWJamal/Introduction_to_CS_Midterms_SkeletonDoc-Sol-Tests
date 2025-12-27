52f71b37306c1331b6fb01f963a44f3d9148347a
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 16:48:19 +0200

    Update midterm_q1_solution.c

--- midterm_B_winter2223_Q1-Q3/Q1/midterm_q1_solution.c
+++ midterm_B_winter2223_Q1-Q3/Q1/midterm_q1_solution.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool midterm_quiz_q1(int num);
+
+// Do not change the main function
+int main() {
+    int num = 0;
+    int student_output = -2;
+
+    scanf(" %d", &num);
+
+    student_output = midterm_quiz_q1(num) ? 1 : 0;
+    printf("%d", student_output);
+
+    return 0;
+}
+
 bool midterm_quiz_q1(int num)
 {
     int lastdigit = num%10;
@@ -15,16 +30,3 @@ bool midterm_quiz_q1(int num)
     return true;
 }
 
-
-// Do not change the main function
-int main() {
-    int num = 0;
-    int student_output = -2;
-
-    scanf(" %d", &num);
-
-    student_output = midterm_quiz_q1(num) ? 1 : 0;
-    printf("%d", student_output);
-
-    return 0;
-}
