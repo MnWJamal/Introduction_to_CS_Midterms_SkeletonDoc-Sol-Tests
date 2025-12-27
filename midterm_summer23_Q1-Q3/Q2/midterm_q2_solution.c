04931c80258705b211a7c2d4cbb46f517d20581d
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 17:35:45 +0200

    Update midterm_q2_solution.c

--- midterm_summer23_Q1-Q3/Q2/midterm_q2_solution.c
+++ midterm_summer23_Q1-Q3/Q2/midterm_q2_solution.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double midterm_q2(double arr[], int n)
-{
-    double min1 = arr[0], min2 = 0;
-    int min2init = 0;
-    for (int i = 1; i < n;i++) {
-        if (arr[i] < min1) {
-            min2 = min1;
-            min1 = arr[i];
-            min2init = 1;
-        }
-        else if ((arr[i] < min2 || !min2init) && arr[i] != min1) {
-            min2 = arr[i];
-            min2init = 1;
-        }
-    }
-    if (!min2init) return min1;
-    return min2;
-}
+double midterm_q2(double arr[], int n);
 
+//DO NOT CHANGE THESE FUNCTIONS
 static void readArray(double *arr, int n)
 {
     for (int i = 0; i < n; i++) {
@@ -56,4 +40,23 @@ int main(void)
 
     free(arr);
     return 0;
+}
+
+double midterm_q2(double arr[], int n)
+{
+    double min1 = arr[0], min2 = 0;
+    int min2init = 0;
+    for (int i = 1; i < n;i++) {
+        if (arr[i] < min1) {
+            min2 = min1;
+            min1 = arr[i];
+            min2init = 1;
+        }
+        else if ((arr[i] < min2 || !min2init) && arr[i] != min1) {
+            min2 = arr[i];
+            min2init = 1;
+        }
+    }
+    if (!min2init) return min1;
+    return min2;
 }
