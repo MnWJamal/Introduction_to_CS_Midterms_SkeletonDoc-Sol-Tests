f87184d665b7e9f96ea02f7aed313c28c997d9c3
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 17:24:23 +0200

    Update midterm_q3_solution.c

--- midterm_summer22_B_Q1-Q3/Q3/midterm_q3_solution.c
+++ midterm_summer22_B_Q1-Q3/Q3/midterm_q3_solution.c
@@ -3,6 +3,34 @@
 #define M 4
 #define N 3
 
+int midterm_quiz_q3(int grades[M][N]); 
+
+//DO NOT CHANGE THESE FUNCTIONS
+static void read_int(int *x) {
+    scanf(" %d", x);
+}
+
+static void fill_grades(int grades[M][N]) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            read_int(&grades[i][j]);
+        }
+    }
+}
+
+static void print_int(int x) {
+    printf("%d", x);
+}
+
+// Do not change the main function
+int main(void) {
+    int grades[M][N];
+    fill_grades(grades);
+    int ans = midterm_quiz_q3(grades);
+    print_int(ans);
+    return 0;
+}
+
 int midterm_quiz_q3(int grades[M][N]){
     double studentAvg[M] = {0};
     int studentHighest[M] = {0};
@@ -45,29 +73,4 @@ int midterm_quiz_q3(int grades[M][N]){
         }
     }
     return -1;
-}
-
-static void read_int(int *x) {
-    scanf(" %d", x);
-}
-
-static void fill_grades(int grades[M][N]) {
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            read_int(&grades[i][j]);
-        }
-    }
-}
-
-static void print_int(int x) {
-    printf("%d", x);
-}
-
-// Do not change the main function
-int main(void) {
-    int grades[M][N];
-    fill_grades(grades);
-    int ans = midterm_quiz_q3(grades);
-    print_int(ans);
-    return 0;
 }
