84dcb30f3656ecad80d717cd0125245d11e03cd7
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 17:25:27 +0200

    Update midterm_q1_skeleton_document.c

--- midterm_summer22_Q1-Q3/Q1/midterm_q1_skeleton_document.c
+++ midterm_summer22_Q1-Q3/Q1/midterm_q1_skeleton_document.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int midterm_quiz_q1(int num); // Replace this line with your function.
+int midterm_quiz_q1(int num);
 
+//DO NOT CHANGE THESE FUNCTIONS
 static int read_int(void) {
     int x = 0;
     scanf(" %d", &x);
@@ -16,5 +17,10 @@ int main(void) {
     int num = read_int();
     int ans = midterm_quiz_q1(num);
     print_int(ans);
+    return 0;
+}
+
+int midterm_quiz_q1(int num) // Replace this with your function.
+{
     return 0;
 }
