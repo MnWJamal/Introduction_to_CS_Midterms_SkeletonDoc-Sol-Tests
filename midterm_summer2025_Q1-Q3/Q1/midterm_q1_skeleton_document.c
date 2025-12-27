b224b41313ae0aa4d9f53b560c1c860a0aad98c5
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 16:58:49 +0200

    Update midterm_q1_skeleton_document.c

--- midterm_summer2025_Q1-Q3/Q1/midterm_q1_skeleton_document.c
+++ midterm_summer2025_Q1-Q3/Q1/midterm_q1_skeleton_document.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int midterm_quiz_q1(void); // Replace this line with your function.
+int midterm_quiz_q1(void);
 
 // Do not change the main function
 int main(void) {
     int student_output = midterm_quiz_q1();
     printf("%d", student_output);
+    return 0;
+}
+
+
+int midterm_quiz_q1(void) // Replace this with your function.
+{
     return 0;
 }
