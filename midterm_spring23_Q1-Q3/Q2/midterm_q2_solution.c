952361a7589000dacd86554ed28a0dde78f0a091
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 16:55:28 +0200

    Update midterm_q2_solution.c

--- midterm_spring23_Q1-Q3/Q2/midterm_q2_solution.c
+++ midterm_spring23_Q1-Q3/Q2/midterm_q2_solution.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void midterm_quiz_q2(char array[], int n, int k);
+
 //DO NOT CHANGE THIS FUNCTION
 void print_k(int k, int amount) {
     printf("Amount of chars that appear %d times: %d\n", k, amount);
 }
-
-
-void midterm_quiz_q2(char array[], int n, int k) {
-    if (k < 0) {
-        print_k(k,0);
-        return;
-    }
-    int charHist[128] = {0};
-    for (int i = 0 ; i < n; i++) {
-        if (array[i] < 0 || array[i] > 127) continue;
-        charHist[(int)array[i]]++;
-    }
-    int ct = 0;
-    for (int i = 0 ; i < 128 ; i++) {
-        if (charHist[i] == k) ct++;
-    }
-    print_k(k,ct);
-}
-
-
-
 
 // Do not change these functions
 void read_int(int *x) {
@@ -56,3 +37,22 @@ int main() {
     free(array);
     return 0;
 }
+
+
+void midterm_quiz_q2(char array[], int n, int k) {
+    if (k < 0) {
+        print_k(k,0);
+        return;
+    }
+    int charHist[128] = {0};
+    for (int i = 0 ; i < n; i++) {
+        if (array[i] < 0 || array[i] > 127) continue;
+        charHist[(int)array[i]]++;
+    }
+    int ct = 0;
+    for (int i = 0 ; i < 128 ; i++) {
+        if (charHist[i] == k) ct++;
+    }
+    print_k(k,ct);
+}
+
