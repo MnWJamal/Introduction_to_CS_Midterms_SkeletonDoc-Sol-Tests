693ea480438b0d97cd85e0f8ab28a42aea4944a1
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 17:40:54 +0200

    Update midterm_q2_skeleton_document.c

--- midterm_winter2425_Q1-Q3/Q2/midterm_q2_skeleton_document.c
+++ midterm_winter2425_Q1-Q3/Q2/midterm_q2_skeleton_document.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
+void midterm_quiz_q2(int n);
+
 //DO NOT CHANGE THIS FUNCTION
 void print_value(int num) {
     printf("%d ",num);
 }
-
-void midterm_quiz_q2(int n); //WRITE YOUR CODE HERE
 
 //DO NOT CHANGE THE MAIN FUNCTION
 int main(void)
@@ -13,4 +14,9 @@ int main(void)
     if (scanf("%d", &n) != 1) return 0;
     midterm_quiz_q2(n);
     return 0;
+}
+
+void midterm_quiz_q2(int n) //Replace this with your function.
+{
+    
 }
