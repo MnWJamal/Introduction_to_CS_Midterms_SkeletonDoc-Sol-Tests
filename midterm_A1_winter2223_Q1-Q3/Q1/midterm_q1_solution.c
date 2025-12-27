e76a9008d2255dd371231628a0ca4c661e93569f
Jamal_Nujedat <jamal.nujedat.safi10@gmail.com>
2025-12-27 16:37:42 +0200

    Update midterm_q1_solution.c

--- midterm_A1_winter2223_Q1-Q3/Q1/midterm_q1_solution.c
+++ midterm_A1_winter2223_Q1-Q3/Q1/midterm_q1_solution.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+int midterm_quiz_q1(double num);
+
+// Do not change the main function
+int main() {
+    double num = 0.0;
+    int student_output = -2;
+
+    // Read a real number
+    scanf(" %lf", &num);
+
+    // Call the required function
+    student_output = midterm_quiz_q1(num);
+
+    // Print exactly what the function returned (no extra newline)
+    printf("%d", student_output);
+    return 0;
+}
+
 int midterm_quiz_q1(double num) {
     if (num < 0) num *= -1;
     int flt1 = num;
@@ -17,18 +35,3 @@ int midterm_quiz_q1(double num) {
     return sum;
 }
 
-// Do not change the main function
-int main() {
-    double num = 0.0;
-    int student_output = -2;
-
-    // Read a real number
-    scanf(" %lf", &num);
-
-    // Call the required function
-    student_output = midterm_quiz_q1(num);
-
-    // Print exactly what the function returned (no extra newline)
-    printf("%d", student_output);
-    return 0;
-}
